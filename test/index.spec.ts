0734b612c117104a7a2134c243e28b6313533267
Melissa Nunes <nunes.melissa.m@gmail.com>
2019-08-03 13:37:01 -0700

    cleaned up index spec

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -2,30 +2,23 @@ import { Mock } from 'ts-mocks';
 import { Map } from 'leaflet';
 import { ILayersMetadata} from '../src/types';
 import { DynamicLayers } from '../src/index';
-
+const layers: ILayersMetadata = [{
+    id: 'testId1',
+    label: 'testLabel1',
+    url: 'www.testUrl1.com',
+    visible: true,
+},
+{
+    id: 'testId3',
+    label: 'testLabel3',
+    url: 'www.testUrl3.com',
+    visible: false,
+}];
 describe('initialization', () => {
-    it('adds enabled layers to the map', () => {
-        const layers: ILayersMetadata = [{
-            id: 'testId1',
-            label: 'testLabel1',
-            url: 'www.testUrl1.com',
-            visible: true,
-        },
-        {
-            id: 'testId2',
-            label: 'testLabel2',
-            url: 'www.testUrl2.com',
-            visible: true,
-        },
-        {
-            id: 'testId3',
-            label: 'testLabel3',
-            url: 'www.testUrl3.com',
-            visible: false,
-        }];
+    it('adds visible layers only to the map', () => {
         const MockMap = new Mock<Map>({addLayer: Mock.ANY_FUNC});
         const map = MockMap.Object;
         const dynamicLayers = new DynamicLayers(map, layers);
-        expect(map.addLayer).toHaveBeenCalledTimes(2);
+        expect(map.addLayer).toHaveBeenCalledTimes(1);
     });
 });
