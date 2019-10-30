b7bc76d3530c0f265f61453591f9f37abb366040
Melissa Nunes <nunes.melissa.m@gmail.com>
2019-10-30 15:14:39 -0600

    Adding a layer to the map after the fact. Resolves #20

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,7 +1,7 @@
 import { Mock } from 'ts-mocks';
 import { Map, Layer, TileLayer } from 'leaflet';
-import { ILayersMetadata, ITilesMetadata, ILeafletLayers} from '../src/dataTypes';
-import { DeclarativeLayers } from '../src/index';
+import { ILayersMetadata, ITilesMetadata, ILeafletLayers, ILayerMetadata} from '../src/dataTypes';
+import { DeclarativeLayers, ILayerReference } from '../src/index';
 import { Feature } from '../node_modules/@types/geojson';
 const geojsonFeature: Feature = {
     type: 'Feature',
@@ -56,14 +56,14 @@ describe('declarative layers', () => {
         });
     });
     describe('access to layers refrences', () => {
-        it('exposes references to added layers', () => {
+        it('exposes references to added layers (visible or not)', () => {
            expect( Object.keys(declarativeLayers.getLayerReferences()).length).toEqual(layers.length);
            expect(declarativeLayers.getLayerReferences().testTileLayer1).toBeDefined();
            expect(declarativeLayers.getLayerReferences().testTileLayer2).toBeDefined();
            expect(declarativeLayers.getLayerReferences().testGeoJsonLayer1).toBeDefined();
         });
     });
-    describe('addingLayers', () => {
+    describe('adding layers initially', () => {
         it('shouldnt be called with undefined values', () => {
             expect(map.addLayer).not.toHaveBeenCalledWith(undefined);
         });
@@ -81,5 +81,83 @@ describe('declarative layers', () => {
                 expect(map.addLayer).toHaveBeenCalledWith(declarativeLayers.getLayerReferences().testGeoJsonLayer1);
             });
         });
+    });
+    describe('adding layers after initialization', () => {
+        let newGeoJsonMetadataVisible: ILayerMetadata;
+        let newGeoJsonMetadataInvisible: ILayerMetadata;
+        let newTileLayerVisible: ILayerMetadata;
+        let newTileLayerInvisible: ILayerMetadata;
+        const newGeojsonFeatureVisible: Feature = {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'Point',
+                coordinates: [-104.99404, 39.75621],
+            },
+        };
+        const newGeojsonFeatureInvisible: Feature = {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'Point',
+                coordinates: [-104.99404, 39.1],
+            },
+        };
+        let layerReferences: ILayerReference;
+        beforeEach(() => {
+            newGeoJsonMetadataVisible = {
+                id: 'testNewGeoJsonLayerVisible',
+                label: 'testNewGeoJsonLayerVisible',
+                visible: true,
+                data: newGeojsonFeatureVisible,
+            };
+            newGeoJsonMetadataInvisible = {
+                id: 'testNewGeoJsonLayerInvisible',
+                label: 'testNewGeoJsonLayerInvisible',
+                visible: false,
+                data: newGeojsonFeatureInvisible,
+            };
+            newTileLayerVisible = {
+                id: 'testNewTileLayerVisible',
+                label: 'testNewTileLayerVisible',
+                url: 'www.testNewTileLayerVisibleUrl.com',
+                visible: true,
+                zIndex: 9,
+            };
+            newTileLayerInvisible = {
+                id: 'testNewTileLayerInvisible',
+                label: 'testNewTileLayerInvisible',
+                url: 'www.testNewTileLayerInvisibleUrl.com',
+                visible: false,
+            };
+            declarativeLayers.addLayer(newGeoJsonMetadataVisible);
+            declarativeLayers.addLayer(newGeoJsonMetadataInvisible);
+            declarativeLayers.addLayer(newTileLayerVisible);
+            declarativeLayers.addLayer(newTileLayerInvisible);
+            layerReferences = declarativeLayers.getLayerReferences();
+        });
+        it('should add a new layer to the references regardless of if it tagged as visible', () => {
+            expect(layerReferences.testNewGeoJsonLayerVisible).toBeDefined();
+            expect(layerReferences.testNewGeoJsonLayerInvisible).toBeDefined();
+            expect(layerReferences.testNewTileLayerVisible).toBeDefined();
+            expect(layerReferences.testNewTileLayerInvisible).toBeDefined();
+        });
+        it('should add the new visible layers to the existing layers on the map', () => {
+            expect(map.addLayer).toHaveBeenCalledWith(layerReferences.testNewGeoJsonLayerVisible);
+            expect(map.addLayer).toHaveBeenCalledWith(layerReferences.testNewTileLayerVisible);
+        });
+        it('should NOT add a new layer with a false visible property to the map', () => {
+            expect(map.addLayer).not.toHaveBeenCalledWith(layerReferences.testNewGeoJsonLayerInvisible);
+            expect(map.addLayer).not.toHaveBeenCalledWith(layerReferences.testNewTileLayerInvisible);
+        });
+        describe('including layer properties', () => {
+            it('should include zindex in added tile layers', () => {
+                expect((layerReferences.testNewTileLayerVisible as TileLayer).options.zIndex)
+                .toEqual((newTileLayerVisible as ITilesMetadata).zIndex);
+                expect((layerReferences.testNewTileLayerInvisible as TileLayer).options.zIndex)
+                .toEqual((newTileLayerInvisible as ITilesMetadata).zIndex);
+            });
+        });
+
     });
 });
