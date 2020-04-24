77b4e8588013173ff27a84e9f67cced265fa87f1
Melissa Nunes <mnunes@sparkgeo.com>
2020-04-24 09:27:21 -0600

    prettier and linting config

--- src/dataTypes.ts
+++ src/dataTypes.ts
@@ -1,44 +1,52 @@
-import * as leaflet from 'leaflet';
+import * as leaflet from 'leaflet'
 // TODO karma complains if import from 'geoJson'. Figure out more elegant solution.
-import * as geojson from '../node_modules/@types/geojson/index';
+import * as geojson from '../node_modules/@types/geojson/index'
 
 export interface IBasicMetadata {
-    label: string;
-    visibleInitially?: boolean;
-    id: string;
-    legend?: string; // path to legend image
+  label: string
+  visibleInitially?: boolean
+  id: string
+  legend?: string // path to legend image
 }
 
 export interface IImageOverlayMetadata extends IBasicMetadata {
-    url: string;
-    bounds: leaflet.LatLngBoundsExpression;
-    options?: leaflet.ImageOverlayOptions;
+  url: string
+  bounds: leaflet.LatLngBoundsExpression
+  options?: leaflet.ImageOverlayOptions
 }
 
-export interface IGeoJsonMetadata  extends IBasicMetadata {
-    data: geojson.GeoJsonObject;
-    options?: leaflet.GeoJSONOptions;
-    generateFeaturePopupContent?(feature: geojson.Feature): leaflet.Content;
+export interface IGeoJsonMetadata extends IBasicMetadata {
+  data: geojson.GeoJsonObject
+  options?: leaflet.GeoJSONOptions
+  generateFeaturePopupContent?(feature: geojson.Feature): leaflet.Content
 }
 
-export interface ITilesMetadata  extends IBasicMetadata {
-    url: string;
-    options?: leaflet.TileLayerOptions;
+export interface ITilesMetadata extends IBasicMetadata {
+  url: string
+  options?: leaflet.TileLayerOptions
 }
 
-export type ILayerMetadata = ITilesMetadata | IGeoJsonMetadata | IImageOverlayMetadata;
+export type ILayerMetadata =
+  | ITilesMetadata
+  | IGeoJsonMetadata
+  | IImageOverlayMetadata
 
 export interface ILayersMetadata extends Array<ILayerMetadata> {}
 
-export type ILeafletLayer = leaflet.TileLayer | leaflet.GeoJSON | leaflet.ImageOverlay;
+export type ILeafletLayer =
+  | leaflet.TileLayer
+  | leaflet.GeoJSON
+  | leaflet.ImageOverlay
 
 // Type Guards
 export const isTilesType = (layer: any): layer is ITilesMetadata => {
-    return (layer.url !== undefined && layer.bounds === undefined);
-};
+  return layer.url !== undefined && layer.bounds === undefined
+}
 export const isGeoJsonType = (layer: any): layer is IGeoJsonMetadata => {
-    return layer.data !== undefined;
-};
-export const isImageOverlayType = (layer: any): layer is IImageOverlayMetadata => {
-    return (layer.bounds !== undefined && layer.url !== undefined);
-};
+  return layer.data !== undefined
+}
+export const isImageOverlayType = (
+  layer: any,
+): layer is IImageOverlayMetadata => {
+  return layer.bounds !== undefined && layer.url !== undefined
+}
